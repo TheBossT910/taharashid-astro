179ac25cf85079717786f63639e9e2034756f463
Taha Rashid <taharashid710192@gmail.com>
2025-07-25 13:29:22 -0400

    chore: added projects

--- src/pages/projects/data.ts
+++ src/pages/projects/data.ts
@@ -2,86 +2,42 @@ import type { ProjectData } from '@/types'
 
 export const projectData: ProjectData = [
   {
-    title: 'Projects Group',
+    title: 'Personal',
     projects: [
       {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
+        text: 'SavePoint - The Modern Game Collection Platform',
+        description: 'A web-first app built to modernize how people collect and manage video games while also helping retro game stores to digitize their inventory, pricing, and storefronts',
+        icon: 'i-carbon-collapse-all',
+        href: 'https://github.com/TheBossT910/savepoint-web',
       },
       {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
+        text: 'Koyomi - AnimeTracker',
+        description: 'A simple iOS app to help you track anime you\'re watching or planning to watch.',
+        icon: 'i-carbon-list-checked',
+        href: 'https://github.com/TheBossT910/AnimeTracker',
+      },
+      {
+        text: 'SyncSlash - 2D Hack n Slash Sidescroller',
+        description: 'A fast-paced 2D hack n slash sidescroller game currently on hiatus, developed with Unity.',
+        icon: 'i-carbon-game-console',
+        href: 'https://github.com/TheBossT910/SlashSync/tree/main',
+      },
+      {
+        text: 'Anki Plugin for Japanese Verb Transivity',
+        description: 'An Anki plugin to parse cards and append the transivity of verbs as "Self-Move" (intransitive) or "Other-Move" (transitive) to thousands of flashcards.',
+        icon: 'i-carbon-language',
+        href: 'https://github.com/TheBossT910/Japanese-Verb-Transivity-Anki-Plugin',
       },
     ],
   },
   {
-    title: 'Projects Group',
+    title: 'University',
     projects: [
       {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-    ],
-  },
-  {
-    title: 'Project Name',
-    projects: [],
-  },
-  {
-    title: 'Projects Group',
-    projects: [
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-    ],
-  },
-  {
-    title: 'Projects Group',
-    projects: [
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
+        text: 'PerfectFit',
+        description: 'A mockup website for an innovative clothing app designed to revolutionize the shopping experience by providing an engaging, immersive, and elegant platform.',
+        icon: 'i-carbon-person',
+        href: 'https://github.com/seg-perfect-fit/perfect-fit-site?tab=readme-ov-file',
       },
     ],
   },
