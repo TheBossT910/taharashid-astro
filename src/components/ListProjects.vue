7d0fccaf5a1c076cbc93f718ccca1a79c3ae48fa
Taha Rashid <taharashid710192@gmail.com>
2026-03-13 19:13:48 -0400

    feat: removed hover experience view
    It was simply bad for readability and usability

--- src/components/ListProjects.vue
+++ src/components/ListProjects.vue
@@ -22,41 +22,7 @@ defineProps<{
 
     <li v-for="project in list" :key="project.text" w-full flex items-center rd-2 class="relative group">
       <a flex items-center target="_blank" :href="project.href" :aria-label="project.text" class="relative">
-        <!-- Display on >SM -->
-        <div class="hidden md:flex">
-          <!-- Preview image -->
-          <img :src="project.image" alt="" class="group:w-100">
-
-          <!-- Icon -->
-          <div class="absolute top-0 ml-2 mr-4 pt-2 ">
-            <i text-4xl inline-block text-black :class="project.icon || 'i-carbon-unknown'" />
-          </div>
-
-          <!-- Display on hover -->
-          <div class="absolute flex flex-col opacity-0  group-hover:opacity-100 bg-gray-200/40 backdrop-blur-sm rounded w-full h-full z-1 transition-all duration-300 ease-in-out">
-            <div class="flex">
-              <!-- Icon -->
-              <div class="top-0 ml-2 mr-4 pt-2">
-                <i text-4xl inline-block text-black :class="project.icon || 'i-carbon-unknown'" />
-              </div>
-
-              <!-- Text & Languages -->
-              <div font-normal lh-tight class="mt-2">
-                <div text-lg text-main text-black>{{ project.text }}</div>
-                <small text-sm text-secondary>{{ project.duration }}</small>
-                <div class="flex flex-wrap gap-x-3 gap-y-1 mt-1 text-sm">
-                  <i v-for="skill in project.skills" :key="skill" text-3xl :class=" skill || 'i-carbon-unknown'" />
-                </div>
-              </div>
-            </div>
-
-            <!-- Desc -->
-            <div class="mt-auto bg-gray-300/50 pt-2 text-black left-0 bottom-0 px-6 pb-4 text-sm">{{ project.description }}</div>
-          </div>
-        </div>
-
-        <!-- Display on <=SM -->
-        <div class="md:hidden rounded">
+        <div class="rounded">
           <!-- Preview image -->
           <img :src="project.image" alt="" class="group:w-100">
 
